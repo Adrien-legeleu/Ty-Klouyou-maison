eb37d66c5a663e9074cf25b12d0774d5836b9c02
Test <adrienlegeleu@gmail.com>
2024-01-09 08:07:04 +0100

    ty klouyou

--- tyklouyou/src/components/Navigation.jsx
+++ tyklouyou/src/components/Navigation.jsx
@@ -3,7 +3,8 @@ import { NavLink } from "react-router-dom";
 
 const Navgiation = ({navigationShow}) => {
   return (
-    <div className="navigation-container" style={{visibility:navigationShow ? "visible" : "hidden"}}>
+   <div className={`${navigationShow ? "navigation-container menu-animation-open" : "navigation-container menu-animation-close"}`}>
+
       <div className="navigation">
         <div className="navigation-sun">
           <div></div>
@@ -13,21 +14,21 @@ const Navgiation = ({navigationShow}) => {
           <div></div>
         </div>
         <div className="navigation-menu">
-          <NavLink to="/" className="menu">
+          <NavLink to="/" className={`${navigationShow ? "menu menu1-anim-open" : "menu menu1-anim-close"}`} style={{visibility: navigationShow ? "hidden" : "visible"}}>
             <h2>Accueil</h2>
             <img src="./assets/img/icons8-arrow-96.png" alt="arrow-left" />
 
           </NavLink>
-          <NavLink className="menu">
+          <NavLink className={`${navigationShow ? "menu menu2-anim-open" : "menu menu2-anim-close"}`} style={{visibility: navigationShow ? "hidden" : "visible"}}>
             <h2>Photos</h2>
             <img src="./assets/img/icons8-arrow-96.png" alt="arrow-left" />
 
           </NavLink>
-          <NavLink className="menu">
+          <NavLink className={`${navigationShow ? "menu menu3-anim-open" : "menu menu3-anim-close"}`} style={{visibility: navigationShow ? "hidden" : "visible"}}>
             <h2>Activités</h2>
             <img src="./assets/img/icons8-arrow-96.png" alt="arrow-left" />
           </NavLink>
-          <NavLink className="menu">
+          <NavLink className={`${navigationShow ? "menu menu4-anim-open" : "menu menu4-anim-close"}`} style={{visibility: navigationShow ? "hidden" : "visible"}}>
             <h2>Contactez</h2>
             <img src="./assets/img/icons8-arrow-96.png" alt="arrow-left" />
           </NavLink>
