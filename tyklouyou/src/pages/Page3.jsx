d4ee6728d8d73bde5cb880d607d3113c900f2f7c
Test <adrienlegeleu@gmail.com>
2024-01-13 14:43:44 +0100

    ty klouyou

--- tyklouyou/src/pages/Page3.jsx
+++ tyklouyou/src/pages/Page3.jsx
@@ -1,42 +1,48 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Page3 = () => {
   return (
     <div className="page3-container">
-      <div className="slider-container">
-        <div className="slider">
-          <img
-            src="./assets/img/extérieur/low-angle-shot-white-church-with-beautiful-flower-garden.jpg"
-            alt="terrasse de la villa ty klouyou"
-          />
-
-          <img
-            src="./assets/img/ile/Belle Île en Mer ( Morbihan Bretagne ).jpg"
-            alt="activités proche de Ty Klouyou (ile)"
-          />
-
-          <img
-            src="./assets/img/vanne/10 Best Places In The North Of France To Visit.jpg"
-            alt="activités proche de Ty Klouyou (vannes)"
-          />
-
-          <img
-            src="./assets/img/salon/interior-with-bid-dining-room-modern-private-house.jpg"
-            alt=" intérieur de la villa ty klouyou"
-          />
-
-          <img
-            src="./assets/img/chambre/luxury-bedroom-suite-resort-high-rise-hotel-with-working-table.jpg"
-            alt="intérieur de la villa ty klouyou"
-          />
-
-          <img
-            src="./assets/img/ile/5 Plages De Rêve Sur Les îles Bretonnes.jpg"
-            alt="activités proche de Ty Klouyou (golfe du morbihan)"
-          />
+      <h2>Des activités à couper le souffle !</h2>
+      <div className="activities-container">
+        <div className="activity">
+          <div className="image">
+            <img
+              src="./assets/img/ile/10 choses à savoir sur le Golfe du Morbihan.jpg"
+              alt="ile proche de villa ty klouyou"
+            />
+          </div>
+          <NavLink to="/" className="link">
+            <h4>Découvrir les Iles Bretonnes</h4>
+          </NavLink>
         </div>
-        <div className="ground">trtrtrt</div>
+        <div className="activity">
+          <div className="image">
+            <img
+              src="./assets/img/vanne/10 Best Places In The North Of France To Visit.jpg"
+              alt="Vannes"
+            />
+          </div>
+          <NavLink to="/" className="link">
+            <h4>Découvrir Vannes</h4>
+          </NavLink>
+        </div>
+        <div className="activity">
+          <div className="image">
+            <img
+              src="./assets/img/ile/5 Plages De Rêve Sur Les îles Bretonnes.jpg"
+              alt="golfe du morbihan"
+            />
+          </div>
+          <NavLink to="/" className="link">
+            <h4>Découvrir le Golfe du Morbihan</h4>
+          </NavLink>
+        </div>
       </div>
+      <button>
+        Voir toutes les activités
+      </button>
     </div>
   );
 };
