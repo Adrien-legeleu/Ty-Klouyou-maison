ed3bec3ff16cabf922eb5b1067bf9e627545dccb
Test <adrienlegeleu@gmail.com>
2024-01-15 15:05:52 +0100

    ty klouyou

--- tyklouyou/src/components/Footer.jsx
+++ tyklouyou/src/components/Footer.jsx
@@ -8,35 +8,35 @@ const Footer = () => {
       <div className="sun-night" onClick={()=>setIsSun(!isSun)}>
       <img src={isSun ? "./assets/img/icons8-sun.svg" : "./assets/img/icons8-symbole-de-la-lune-60.png"} alt="sun-moon-tyKlouyou" />
       </div>
-      <div className="logo-container">
-      <h3>Designed by <a href="#">W3bLuxury</a></h3>
+      <div className="logo-container" style={{color: isSun ? "black" : "white" , boxShadow:isSun ? "inset 0 0 10px rgba(0, 0, 0, 0.151)" : " 0 0 15px rgba(255, 255, 255, 0.25)"}}>
+      <h3>Designed by <a href="#" style={{color: isSun ? "black" : "white"}}>W3bLuxury</a></h3>
       <p>création de site web design</p>
-      <a href="#">en savoir plus</a>
+      <a href="#" style={{color: isSun ? "black" : "white"}}>en savoir plus</a>
       </div>
       <div className="link-container">
         
-        <ul className="legal">
+        <ul className="legal" style={{color: isSun ? "black" : "white"}} >
           <li>Règlement Intérieur</li>
           <li>Mentions légales</li>
           <li>© WebLuxury Design 2024</li>
         </ul>
         <ul className="pages">
-          <NavLink to="/">
+          <NavLink to="/" style={{color: isSun ? "black" : "white"}}>
             <li>Accueil</li>
           </NavLink>
-          <NavLink to="/">
+          <NavLink to="/" style={{color: isSun ? "black" : "white"}}>
             <li>Photos</li>
           </NavLink>
-          <NavLink to="/">
+          <NavLink to="/" style={{color: isSun ? "black" : "white"}}>
             <li>Activités</li>
           </NavLink>
-          <NavLink to="/">
+          <NavLink to="/" style={{color: isSun ? "black" : "white"}}>
             <li>Contact</li>
           </NavLink>
         </ul>
         <div className="btn">
         <button>
-          Contactez-nous
+          Contactez Ty Klouyou
         </button>
         </div>
       </div>
