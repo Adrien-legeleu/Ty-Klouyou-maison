dee80358e4ba8872900a68319ae6dc1fca75d07f
Test <adrienlegeleu@gmail.com>
2024-01-20 08:02:53 +0100

    ty klouyou

--- tyklouyou/src/pages/Act2.jsx
+++ tyklouyou/src/pages/Act2.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Act2 = ({ data }) => {
+    const [isAutoPlay , setIsAutoPlay]=useState(false)
+
+
   return (
     <div className="slide">
       <div className="text">
@@ -8,7 +11,9 @@ const Act2 = ({ data }) => {
         <p> {data.text} </p>
       </div>
       <div className="video-container">
-        <iframe className="video" src={data.src} title="YouTube video player" ></iframe>
+        <div className="video" onMouseEnter={()=>setIsAutoPlay(true)} onMouseLeave={()=>setIsAutoPlay(false)}>
+            <iframe className="iframe" src={`${data.src} ${isAutoPlay ? "?rel=0&showinfo=0&controls=1&autoplay=1" : "?rel=0&showinfo=0&controls=1"}`} title="YouTube video player" allowFullScreen  autoPlay></iframe>
+        </div>
       </div>
     </div>
   );
