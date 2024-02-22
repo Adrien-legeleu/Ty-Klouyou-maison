b06b62bba18dcbcc86b82640608e669f081fd31d
Test <adrienlegeleu@gmail.com>
2024-02-22 20:09:48 +0100

    ty klouyogit push

--- tyklouyou/src/pages/Act2.jsx
+++ tyklouyou/src/pages/Act2.jsx
@@ -1,6 +1,24 @@
+import { useGSAP } from "@gsap/react";
+import {gsap} from "gsap";
 import React from "react";
 
+
 const Act2 = ({ data }) => {
+
+
+  useGSAP(() => {
+  gsap.to(".video", {
+    transform:'translate(0)',
+    scrollTrigger: {
+      trigger: ".video", // Le déclencheur est la vidéo
+      start: "right right", // Commencer l'animation lorsque la droite de la vidéo atteint la droite de la fenêtre
+      end: "+=100%", // Facultatif : vous pouvez spécifier quand terminer l'animation
+      scrub: 2,
+      markers: true // Pour le débogage, retirez-le si vous ne l'utilisez pas
+    }
+  });
+});
+
 
   return (
     <div className="slide">
